ceab5452726b51e55706767859765bad1700f18c
Adil Munawar <adilfromavengers@gmail.com>
2025-10-12 10:46:56 +0000

    I want the old automatic animation effect for this our process section w

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8de47a2dcfded41e"; }
+/******/ 		__webpack_require__.h = function() { return "ea3d1b03dce09710"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
