a6ff70467895d9280ab015ecea063ae5a14d31d4
Adil Munawar <adilfromavengers@gmail.com>
2025-10-16 13:19:37 +0000

    build for vercel fixed other issues

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e917a1e67621f456"; }
+/******/ 		__webpack_require__.h = function() { return "0708c75d75cfe2a8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
